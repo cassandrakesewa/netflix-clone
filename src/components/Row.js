d6462ec448377a29467cbbd2277c7d89fd491a4d
cassandra <cassandra.kesewa@gmail.com>
2020-08-19 09:16:15 +0000

    initial commit of netflix clone frontend project

---
+++ src/components/Row.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import "./Row.css";
+
+const imgBaseUrl = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {/* Movie Image contents in a container */}
+        {movies.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`${imgBaseUrl}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
